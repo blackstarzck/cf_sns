b6fa522ce7a1b754daa22ec2a0c006be15c65b2a
chanki <bucheongosok@gmail.com>
2024-07-10 22:50:57 +0900

    update gitignore list

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -6,8 +6,9 @@ import { Server, Socket } from "socket.io";
 })
 export class ChatsGateway implements OnGatewayConnection {
     @WebSocketServer()
-    server: Server;
+    server: Server; // socket.io 서버 객체.
 
+    // web socket 이 연결되면 실행되는 메서드
     handleConnection(socket: Socket, ...args: any[]) {
         console.log(`on connect called: ${socket.id}`);
     }
@@ -30,6 +31,13 @@ export class ChatsGateway implements OnGatewayConnection {
         @ConnectedSocket() socket: Socket
     ) {
         // console.log("message: ", message);
+
+        // this.server.emit('receive_message', 'hello from server~!'); // ← 모든 클라이언트에게 메시지를 보낸다
+
+        // 서버에서 보낸(emit) 메시지를 listening 하기 위해서는,
+        // 포스트맨에서 Events 탭에 receive_message 이벤트를 등록해야 한다.
+
+        // ↓ namespace: chats 으로 연결되어 있고 chatId 방에 있는 모든 클라이언트에게 메시지를 보냅니다.
         this.server.in(
             message.chatId.toString()
         ).emit('receive_message', message.message); // namespace: chats 으로 연결되어 있는 모든 클라이언트에게 메시지를 보냅니다.
